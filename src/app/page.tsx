ec779c83fa70a5f965a9bc2eb37de31cc44328b5
Gomes_N <gomes.nicolas95@gmail.com>
2024-04-02 00:53:59 +0200

    Edit profil picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.big_picture} style={{backgroundImage: `url(${bigPicture.src})`}} />
       <div className={styles.profil_picture}>
+        <div className={styles.name}>Gomes Nicolas</div>
         <div className={styles.picture} style={{backgroundImage: `url(${profilePicture.src})`}} />
+        <div className={styles.job}>Full Stack developer</div>
       </div>
     </main>
   );
